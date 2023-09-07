5b72cd554638443fd3de539106661d43d92da1fa
Dawid <dawid1234x@wp.pl>
2023-09-07 21:53:09 +0200

    Data fetch

--- synonym-finder/src/App.tsx
+++ synonym-finder/src/App.tsx
@@ -1,28 +1,42 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+
+type Synonym = {
+  word: string;
+  score: number;
+};
 
 function App() {
-  const [word, setWord] = useState<string>()
+  const [word, setWord] = useState<string>('');
+  const [synonyms, setSynonyms] = useState<Synonym[]>([]);
 
-  const handeFetchSynonym = (e) => {
+  const handleFetchSynonyms = (e: React.FormEvent) => {
     e.preventDefault();
-
-  }
+    fetch(`https://api.datamuse.com/words?rel_syn=${word}`)
+      .then((response) => response.json())
+      .then(setSynonyms)
+      .catch((error) => console.error('Error fetching synonyms:', error));
+  };
 
   return (
-    <>
-      <div>
-      <form method="POST" action="/url" onSubmit={handeFetchSynonym}>
+    <div className='App'>
+      <form onSubmit={handleFetchSynonyms}>
         <label htmlFor='word-input'>Your Word</label>
-        <input id='word-input'
+        <input
+          id='word-input'
           value={word}
           onChange={(e) => setWord(e.target.value)}
-        ></input>
+        />
         <button>Submit</button>
       </form>
-      </div>
-    </>
-  )
+      <ul>
+        {synonyms &&
+          synonyms.map((synonym, index) => (
+            <li key={index}>{synonym.word}</li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
